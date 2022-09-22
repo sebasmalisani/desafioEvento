b56616f992d68ff806190378cc36885b673f87c0
sebasmalisani <sebas.malisani@gmail.com>
2022-09-22 18:33:23 -0300

    Agregue cantidad

--- desafioevento.js
+++ desafioevento.js
@@ -4,34 +4,39 @@ const modalContainer = document.getElementById('modal-container')
 
 const productos = [
     {
-        id: 1,
-        nombre: "Yerba",
-        precio: 500,
-        img: 'https://jumboargentina.vtexassets.com/arquivos/ids/609099/Yerba-Mate-Playadito-Suave-X500gr-1-854543.jpg?v=637388478795030000'
+        "id": 1,
+        "nombre": "Yerba",
+        "precio": 500,
+        "img": 'https://jumboargentina.vtexassets.com/arquivos/ids/609099/Yerba-Mate-Playadito-Suave-X500gr-1-854543.jpg?v=637388478795030000',
+        "cantidad": 1,
     },
     {
         id: 2,
         nombre: "Coca",
         precio: 350,
-        img: 'https://www.pngall.com/wp-content/uploads/2016/04/Coca-Cola-PNG-Clipart.png'
+        img: 'https://www.pngall.com/wp-content/uploads/2016/04/Coca-Cola-PNG-Clipart.png',
+        "cantidad": 1,
     },
     {
         id: 3,
         nombre: "Papas-Fritas",
         precio: 500,
-        img: 'https://www.martinezydiezsl.com/wp-content/uploads/2018/03/lays_al_punto_de_sal-web2.png'
+        img: 'https://www.martinezydiezsl.com/wp-content/uploads/2018/03/lays_al_punto_de_sal-web2.png',
+        "cantidad": 1,
     },
     {
         id: 4,
         nombre: "Mermelada",
         precio: 300,
-        img: 'https://www.pngmart.com/files/10/Jam-PNG-HD.png'
+        img: 'https://www.pngmart.com/files/10/Jam-PNG-HD.png',
+        "cantidad": 1,
     },
     {
         id: 5,
         nombre: "Patitas",
         precio: 750,
-        img: 'https://www.granjadelsol.com.ar/wp-content/uploads/2017/10/patitas-400g.png.webp'
+        img: 'https://www.granjadelsol.com.ar/wp-content/uploads/2017/10/patitas-400g.png.webp',
+        "cantidad": 1,
     },
 ]
 
@@ -55,13 +60,26 @@ productos.forEach((producto) => {
     content.append(comprar);
 
     comprar.addEventListener("click", () => {
-        carrito.push({
-            id: producto.id,
-            img: producto.img,
-            nombre: producto.nombre,
-            precio: producto.precio,
-        });
-        alert("Tu producto se agrego al carrito");
+        let agregarProducto = productos.find(p => p.id === producto.id);
+        let existe = carrito.find(elemento => elemento.id === producto.id)
+        if (existe){
+            existe.cantidad++;
+            console.log(carrito);
+            alert(`El producto ${producto.nombre} fue agregada nuevamente al carrito.`)
+        } else{
+            agregarProducto.cantidad = 1;
+            carrito.push(agregarProducto);
+            console.log(carrito);
+            alert(`Producto agregado correctamente al carrito.`);
+        }
+        // carrito.push({
+        //     id: producto.id,
+        //     img: producto.img,
+        //     nombre: producto.nombre,
+        //     precio: producto.precio,
+        // });
+        // alert("Tu producto se agrego al carrito");
+        
     });
 });
 
@@ -94,13 +112,14 @@ verCarrito.addEventListener("click", () => {
             <img src="${producto.img}">
             <h3>${producto.nombre}</h3>
             <p>${producto.precio} $</p>
+            <p class="cantidad">Cantidad: ${producto.cantidad}</p>
         `;
         
     modalContainer.append(carritoContent)
     });
     
 
-    const total = carrito.reduce((acc, el) => acc + el.precio, 0);
+    const total = carrito.reduce((acc, el) => acc + el.precio * el.cantidad, 0);
 
     const totalComprado = document.createElement("div");
     totalComprado.className = "total-content";
