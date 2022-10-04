c0ce375dd4ee9bf5005c363d5d64380a7a131525
sebasmalisani <sebas.malisani@gmail.com>
2022-10-04 19:01:45 -0300

    operadores avanzados

--- desafioevento.js
+++ desafioevento.js
@@ -4,41 +4,72 @@ const modalContainer = document.getElementById('modal-container')
 
 const productos = [
     {
-        "id": 1,
-        "nombre": "Yerba",
-        "precio": 500,
-        "img": 'https://jumboargentina.vtexassets.com/arquivos/ids/609099/Yerba-Mate-Playadito-Suave-X500gr-1-854543.jpg?v=637388478795030000',
-        "cantidad": 1,
+        id: 1,
+        nombre: "Yerba",
+        precio: 500,
+        img: 'https://jumboargentina.vtexassets.com/arquivos/ids/609099/Yerba-Mate-Playadito-Suave-X500gr-1-854543.jpg?v=637388478795030000',
+        cantidad: 1,
     },
     {
         id: 2,
         nombre: "Coca",
         precio: 350,
         img: 'https://www.pngall.com/wp-content/uploads/2016/04/Coca-Cola-PNG-Clipart.png',
-        "cantidad": 1,
+        cantidad: 1,
     },
     {
         id: 3,
         nombre: "Papas-Fritas",
         precio: 500,
         img: 'https://www.martinezydiezsl.com/wp-content/uploads/2018/03/lays_al_punto_de_sal-web2.png',
-        "cantidad": 1,
+        cantidad: 1,
     },
     {
         id: 4,
         nombre: "Mermelada",
         precio: 300,
         img: 'https://www.pngmart.com/files/10/Jam-PNG-HD.png',
-        "cantidad": 1,
+        cantidad: 1,
     },
     {
         id: 5,
         nombre: "Patitas",
         precio: 750,
         img: 'https://www.granjadelsol.com.ar/wp-content/uploads/2017/10/patitas-400g.png.webp',
-        "cantidad": 1,
+        cantidad: 1,
     },
 ]
+
+
+
+const desestructurar = (productos) => {
+    const [{id, nombre, precio}] = productos;
+    console.log(nombre);
+};
+
+desestructurar(productos);
+
+const [a, b] = productos;
+console.log(a, b);
+
+
+console.log(...productos)
+
+let numero = 11;
+let numeroLiteral = numero == 5 ? 'cinco' :
+                    numero == 7 ? 'siete' :
+                    numero == 11 ? 'once' :
+                    numero == 15 ? 'quince' :
+                    'otro numero';
+console.log( numeroLiteral );
+
+
+// const [{ id, nombre, precio, }] = productos;
+// console.log(id);
+// console.log(nombre);
+// console.log(precio);
+
+
 
 let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
